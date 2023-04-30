0e2844d52202ff2701b0465e66b90b713704b48a
Maxi Quoß <maxi@quoss.org>
2023-04-30 22:02:33 +0200

    make script sunrise/sunset based & check for feh

--- setbg.sh
+++ setbg.sh
@@ -1,36 +1,58 @@
 #!/bin/bash
 
-DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-FILES=$(find $DIR -type f | grep XPP | sort)
-TIMEOUT_MIN=0
+TIMEOUT_MIN=15
 
-setBackground()
-{
-  if test "`find ~/.fehbg -mmin +$TIMEOUT_MIN`"; then
-    FILE=$(echo "$FILES" | grep "M4x_$1_" | grep -v NoMoon | shuf -n 1)
-    echo "SET NEW BACKGROUND: $FILE"
-    feh --bg-fill $FILE
-  fi
+function setBackground() {
+  all_files=$(find "$(dirname "$0")" -type f | grep XPP | sort)
+  new_bg=$(echo "$all_files" | grep "M4x_$1_" | grep -v NoMoon | shuf -n 1)
+  echo "Set new background for $1: $new_bg"
+  feh --bg-fill "$new_bg"
 }
 
+function round_hour() {
+    hour=$(date -d "$1" +%H)
+    if [[ $(date -d "$1" +%M) -ge 30 || $(date -d "$1" +%S) -ge 30 ]]; then
+        hour=$((hour + 1))
+    fi
+    if [[ $hour -eq 24 ]]; then
+        hour=0
+    fi
+    echo "$hour"
+}
+
+if ! command -v feh &> /dev/null; then
+    echo "feh is not installed"
+    exit
+fi
+
 while true; do
+  hour_now=$(date +%H)
 
-  HOUR=$(date +%H)
-
-  if test $HOUR -gt 20 -o $HOUR -le 5; then
-    setBackground "Night"
-  elif test $HOUR -gt 17; then
-    setBackground "Sunset"
-  elif test $HOUR -gt 10; then
-    setBackground "Day"
-  elif test $HOUR -gt 7; then
-    setBackground "Morning"
-  elif test $HOUR -gt 5; then
-    setBackground "Sunrise"
+  # check background creation date for timeout
+  if [ -f "$HOME/.fehbg" ]; then
+    if find ~/.fehbg -mmin +"$TIMEOUT_MIN"; then
+      echo "Sleeping $TIMEOUT_MIN min"
+      sleep $(("$TIMEOUT_MIN"*60))
+      continue
+    fi
   fi
-  
-  TIMEOUT_MIN=15
 
-  sleep 5
+  # get sunrise and sunset hours
+  sunrise=$(curl -s "https://wttr.in?format=%S")
+  sunset=$(curl -s "https://wttr.in?format=%s")
+  rounded_sunrise=$(round_hour "$sunrise")
+  rounded_sunset=$(round_hour "$sunset")
+
+  if [ "$hour_now" -eq "$rounded_sunrise" ]; then
+    setBackground "Sunrise"
+  elif [ "$hour_now" -gt "$rounded_sunrise" ] && [ "$hour_now" -le "$(("$rounded_sunrise"+2))" ]; then
+    # "morning" is 2 hours after sunrise
+    setBackground "Morning"
+  elif [ "$hour_now" -gt "$(("$rounded_sunrise"+2))" ] && [ "$hour_now" -lt "$rounded_sunset" ]; then
+    setBackground "Day"
+  elif [ "$hour_now" -eq "$rounded_sunset" ]; then
+    setBackground "Sunset"
+  else
+    setBackground "Night"
+  fi
 done
-
